c82902110a4925807a62de64fe47b8b5f125ec2b
elderls <95724731+elderls@users.noreply.github.com>
2022-10-17 18:18:14 +1400

    novo

---
+++ Conhecimento/src/Exercicios/Data.java
@@ -0,0 +1,7 @@
+package Exercicios;
+
+public class Data {
+    int dia;
+    int mes;
+    int ano;
+}
---
+++ Conhecimento/src/Exercicios/DataTeste.java
@@ -0,0 +1,19 @@
+package Exercicios;
+
+public class DataTeste {
+    public static void main(String[] args) {
+        Data p1 = new Data(); 
+        p1.dia = 10;
+        p1.mes = 12;
+        p1.ano = 1984;
+
+        var p2 = new Data();
+        p2.ano = 1984;
+        p2.mes = 07;
+        p2.dia = 10;
+
+
+        System.out.print(p1);
+
+    }
+}
