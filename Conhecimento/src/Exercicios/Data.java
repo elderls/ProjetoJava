21ea441c4544aab0304bd9335029d3beb02107b9
elderls <95724731+elderls@users.noreply.github.com>
2022-10-25 17:54:37 +1400

    data

--- Conhecimento/src/Exercicios/Data.java
+++ Conhecimento/src/Exercicios/Data.java
@@ -5,9 +5,22 @@ public class Data {
     int mes;
     int ano;
 
+    Data() {
+        dia = 01;
+        mes = 01;
+        ano = 1970; 
+     }
+
+    Data(int dianew, int mesnew, int anonew) {
+        dia = dianew;
+        mes = mesnew;
+        ano = anonew; 
+    }
 
     String obterFormatacao(){
 
         return String.format("O dia do processamento é %d/%d/%d",dia, mes, ano) ;
     } 
+
+
 }
--- Conhecimento/src/Exercicios/DataTeste.java
+++ Conhecimento/src/Exercicios/DataTeste.java
@@ -3,19 +3,17 @@ package Exercicios;
 public class DataTeste {
     public static void main(String[] args) {
         Data p1 = new Data(); 
-        p1.dia = 10;
-        p1.mes = 12;
-        p1.ano = 1984;
+//        p1.dia = 10;
+//        p1.mes = 12;
+//        p1.ano = 1984;
 
-        var p2 = new Data();
-        p2.ano = 1984;
-        p2.mes = 07;
-        p2.dia = 10;
+        var p2 = new Data(10,07,1984);
+
 
 
         System.out.println("data " + p1.dia +  p1.mes  + p1.ano );
         System.out.println("data " + p1.dia + "/" + p1.mes + "/" + p1.ano );
-        System.out.println(p1.obterFormatacao());
+        System.out.println(p2.obterFormatacao());
 
     }
 }
