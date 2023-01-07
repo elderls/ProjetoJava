d8ac985a678045817deb8d57ca4a86b09dd3d0ee
elderls <elder.lima.souza@outlook.com>
2023-01-07 11:15:39 -0300

    Atualizar

--- Conhecimento/src/Exercicios/Exercicios.operadores/Exercicios/Exercicios/operadores/logicas.java
+++
@@ -1,7 +0,0 @@
-package Exercicios.Exercicios.operadores;
-
-public class logicas {
-    public static void main(String[] args) {
-        
-    }
-}
--- Conhecimento/src/Exercicios/MaiorNumero.java
+++
@@ -1,6 +0,0 @@
-package MaiorNumero;
-
-public class MaiorNumero {
-    public static void main(String[] args) {
-
-}
